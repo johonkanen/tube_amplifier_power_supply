14abf08ab6ecf8ac89f24312aedac98ae7a79253
Jari Honkanen <Jari.Honkanen@lut.fi>
2019-06-09 14:17:04 +0300

    add phys pin location constraints file

--- spartan_7/npm_test.tcl
+++ spartan_7/npm_test.tcl
@@ -14,35 +14,46 @@ if {[llength $files] != 0} {
 # read vhdl files
 read_vhdl -vhdl2008 [ glob s7_specifics.vhd ]
 read_vhdl -vhdl2008 [ glob ../source/top/top.vhd ]
+
+# dhb sources
 read_vhdl -vhdl2008 [ glob ../source/dhb/dhb_pkg.vhd ]
+read_vhdl -vhdl2008 [ glob ../source/dhb/phase_modulator.vhd ]
+
+# ad sources
+read_vhdl -vhdl2008 [ glob ../source/ext_ad/ad_ctrl.vhd ]
+read_vhdl -vhdl2008 [ glob ../source/ext_ad/ext_ad_control.vhd ]
+read_vhdl -vhdl2008 [ glob ../source/ext_ad/spi3w_ads7056_driver.vhd ]
+read_vhdl -vhdl2008 [ glob ../source/ext_ad/ad_bus_driver.vhd ]
+read_vhdl -vhdl2008 [ glob ../source/ext_ad/ad_bus_pkg.vhd ]
+read_vhdl -vhdl2008 [ glob ../source/ext_ad/ext_ad_spi3w.vhd ]
+
 read_vhdl -vhdl2008 [ glob ../source/heater_ctrl/*.vhd ]
 read_vhdl -vhdl2008 [ glob ../source/led_driver/*.vhd ]
-read_vhdl -vhdl2008 [ glob ../source/ext_ad/ad_bus_pkg.vhd ]
 read_vhdl -vhdl2008 [ glob ../source/onboard_ad_control/*.vhd ]
 read_vhdl -vhdl2008 [ glob ../source/pfc_ctrl/*.vhd ]
 read_vhdl -vhdl2008 [ glob ../source/sw_supply_control/*.vhd ]
 read_vhdl -vhdl2008 [ glob ../source/system_control/*.vhd ]
 read_vhdl -vhdl2008 [ glob ../source/uart/*.vhd ]
 read_ip .srcs/sources_1/ip/main_pll/main_pll.xci
-# read_xdc vivado_specifics/constraints.xdc
+read_xdc constraints/pinouts.xdc
 
- synth_design -top top -part $partNum
-# write_checkpoint -force $outputDir/post_synth.dcp
-# report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
-# report_utilization -file $outputDir/post_synth_util.rpt
+synth_design -top top -part $partNum
+write_checkpoint -force $outputDir/post_synth.dcp
+report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
+report_utilization -file $outputDir/post_synth_util.rpt
 
-# opt_design
-# place_design
-# report_clock_utilization -file $outputDir/clock_util.rpt
+ opt_design
+ place_design
+ report_clock_utilization -file $outputDir/clock_util.rpt
 
-#get timing violations and run optimizations if needed
-# if {[get_property SLACK [get_timing_paths -max_paths 1 -nworst 1 -setup]] < 0} {
-#  puts "Found setup timing violations => running physical optimization"
-#  phys_opt_design
-# }
-# write_checkpoint -force $outputDir/post_place.dcp
-# report_utilization -file $outputDir/post_place_util.rpt
-# report_timing_summary -file $outputDir/post_place_timing_summary.rpt
+get timing violations and run optimizations if needed
+ if {[get_property SLACK [get_timing_paths -max_paths 1 -nworst 1 -setup]] < 0} {
+  puts "Found setup timing violations => running physical optimization"
+  phys_opt_design
+ }
+ write_checkpoint -force $outputDir/post_place.dcp
+report_utilization -file $outputDir/post_place_util.rpt
+report_timing_summary -file $outputDir/post_place_timing_summary.rpt
 
 #Route design and generate bitstream
 # route_design -directive Explore
