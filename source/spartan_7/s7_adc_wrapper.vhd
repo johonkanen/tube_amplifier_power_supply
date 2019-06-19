696d1ea9aafd4dfd955eb9af9119d698f9a5632f
Jari Honkanen <jari.honkanen@lut.fi>
2019-06-19 17:33:15 +0300

    add adc wrapper to project

---
+++ source/spartan_7/s7_adc_wrapper.vhd
@@ -0,0 +1,107 @@
+library ieee;
+    use ieee.std_logic_1164.all;
+        use ieee.numeric_std.all;
+
+entity adc_wrapper is
+	generic(
+				g_u8_clk_cnt : unsigned(7 downto 0);
+				g_u8_clks_per_conversion : unsigned(7 downto 0);
+				g_sh_counter_latch : unsigned(7 downto 0)
+			);
+	port( 
+			si_spi_clk 	 : in std_logic; 
+            si_pll_lock : in std_logic;
+			 
+			-- physical signals to ext ad converter
+			po_spi_cs 	 : out std_logic;
+			po_spi_clk_out : out std_logic;
+			pi_spi_serial : in std_logic; 
+ 
+			si_spi_start : in std_logic; 
+			 
+			-- ext spi control signals
+			s_spi_busy	 : out std_logic; 
+			-- output signal indicating word is ready to be read 
+			so_spi_rdy	 : out std_logic; 
+			-- output signal indicating sampling is done
+			so_sh_rdy	 : out std_logic; 
+			-- output buffer
+			b_spi_rx : out std_logic_vector(15 downto 0)  
+		);	
+end entity; 
+
+architecture rtl of adc_wrapper is
+
+component ext_ad_spi3w is
+	generic(
+				g_u8_clk_cnt : unsigned(7 downto 0);
+				g_u8_clks_per_conversion : unsigned(7 downto 0);
+				g_sh_counter_latch : unsigned(7 downto 0)
+			);
+	port( 
+		si_spi_clk 	 : in std_logic; 
+		 
+		-- physical signals to ext ad converter
+		po_spi_cs 	 : out std_logic;
+		po_spi_clk_out : out std_logic;
+		pi_spi_serial : in std_logic; 
+
+		si_spi_start	 : in std_logic; 
+		 
+		-- ext spi control signals
+		s_spi_busy	 : out std_logic; 
+		-- output signal indicating word is ready to be read 
+		so_spi_rdy	 : out std_logic; 
+		-- output signal indicating sampling is done
+		so_sh_rdy	 : out std_logic; 
+		-- output buffer
+		b_spi_rx : out std_logic_vector(15 downto 0)  
+	    );	
+end component; 
+
+
+
+component spi3w_ads7056_driver is
+	generic(
+				g_u8_clk_cnt : unsigned(7 downto 0);
+				g_u8_clks_per_conversion : unsigned(7 downto 0);
+				g_sh_counter_latch : unsigned(7 downto 0)
+			);
+	port( 
+			si_spi_clk 	 : in std_logic; 
+            si_pll_lock : in std_logic;
+			 
+			-- physical signals to ext ad converter
+			po_spi_cs 	 : out std_logic;
+			po_spi_clk_out : out std_logic;
+			pi_spi_serial : in std_logic; 
+ 
+			si_spi_start	 : in std_logic; 
+			 
+			-- ext spi control signals
+			s_spi_busy	 : out std_logic; 
+			-- output signal indicating word is ready to be read 
+			so_spi_rdy	 : out std_logic; 
+			-- output signal indicating sampling is done
+			so_sh_rdy	 : out std_logic; 
+			-- output buffer
+			b_spi_rx : out std_logic_vector(15 downto 0)  
+		);	
+end component; 
+
+begin
+
+onboard_adc : ext_ad_spi3w
+    generic map(8d"2",8d"18",8d"12")
+    port map(
+			si_spi_clk,
+			po_spi_cs,
+			po_spi_clk_out,
+			pi_spi_serial, 
+			si_spi_start,
+			s_spi_busy,
+			so_spi_rdy,
+			so_sh_rdy,
+			b_spi_rx);
+
+end rtl;
