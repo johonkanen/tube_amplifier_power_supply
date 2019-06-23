bb980c9d589f84f23e2296fc6997a87cb2b278fc
Jari Honkanen <jari.honkanen@lut.fi>
2019-06-23 10:05:25 +0300

    successfully add multiplier ip with tcl script

--- spartan_7/tcl_scripts/ipgen_main_pll.tcl
+++ spartan_7/tcl_scripts/ipgen_main_pll.tcl
@@ -2,17 +2,17 @@
 set partNum xc7s15ftgb196-2
 create_project -in_memory -part $partNum
 
-create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name pll_1x256mhz -dir ../.srcs/sources_1/ip
+create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name pll_1x256mhz -dir ../ip
 
 set_property -dict [list CONFIG.Component_Name {pll_1x256mhz} CONFIG.PRIM_IN_FREQ {32.000} CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {256.000} CONFIG.USE_RESET {false} CONFIG.CLKIN1_JITTER_PS {312.5} CONFIG.MMCM_CLKFBOUT_MULT_F {31.000} CONFIG.MMCM_CLKIN1_PERIOD {31.250} CONFIG.MMCM_CLKIN2_PERIOD {10.0} CONFIG.MMCM_CLKOUT0_DIVIDE_F {3.875} CONFIG.CLKOUT1_JITTER {173.096} CONFIG.CLKOUT1_PHASE_ERROR {203.212}] [get_ips pll_1x256mhz]
 
-generate_target {instantiation_template} [get_files ../.srcs/sources_1/ip/pll_1x256mhz/pll_1x256mhz.xci]
+generate_target {instantiation_template} [get_files ../ip/pll_1x256mhz/pll_1x256mhz.xci]
 
-set_property generate_synth_checkpoint false [get_files  ../.srcs/sources_1/ip/pll_1x256mhz/pll_1x256mhz.xci]
+set_property generate_synth_checkpoint false [get_files  ../ip/pll_1x256mhz/pll_1x256mhz.xci]
 
-generate_target all [get_files  ../.srcs/sources_1/ip/pll_1x256mhz/pll_1x256mhz.xci]
+generate_target all [get_files  ../ip/pll_1x256mhz/pll_1x256mhz.xci]
 
-export_ip_user_files -of_objects [get_files ../.srcs/sources_1/ip/pll_1x256mhz/pll_1x256mhz.xci] -no_script -sync -force -quiet
+export_ip_user_files -of_objects [get_files ../ip/pll_1x256mhz/pll_1x256mhz.xci] -no_script -sync -force -quiet
 
-export_simulation -of_objects [get_files ../.srcs/sources_1/ip/pll_1x256mhz/pll_1x256mhz.xci] -directory .ip_user_files/sim_scripts -ip_user_files_dir .ip_user_files -ipstatic_source_dir .ip_user_files/ipstatic -lib_map_path [list {modelsim=./.cache/compile_simlib/modelsim} {questa=./.cache/compile_simlib/questa} {riviera=./.cache/compile_simlib/riviera} {activehdl=./.cache/compile_simlib/activehdl}] -use_ip_compiled_libs -force -quiet
+export_simulation -of_objects [get_files ../ip/pll_1x256mhz/pll_1x256mhz.xci] -directory ../ip/.ip_user_files/sim_scripts -ip_user_files_dir ../ip/.ip_user_files -ipstatic_source_dir ../ip/.ip_user_files/ipstatic -lib_map_path [list {modelsim=./.cache/compile_simlib/modelsim} {questa=./.cache/compile_simlib/questa} {riviera=./.cache/compile_simlib/riviera} {activehdl=./.cache/compile_simlib/activehdl}] -use_ip_compiled_libs -force -quiet
 
--- spartan_7/tcl_scripts/ipgen_mult_18x18.tcl
+++ spartan_7/tcl_scripts/ipgen_mult_18x18.tcl
@@ -1,12 +1,43 @@
 
 set partNum xc7s15ftgb196-2
 create_project -in_memory -part $partNum
+# Check IP
+    # if the IP files exist, we already generated the IP, so we can just
+    # read the ip definition (.xci)
+        # IP folder does not exist. Create IP folder
+if { [file isdirectory "../IP/combi_mult_18x18/"] } {
+    read_ip ../IP/combi_mult_18x18/combi_mult_18x18.xci
+} else {
+       file mkdir ../IP
+       create_ip -name mult_gen -vendor xilinx.com -library ip \
+       -version 12.0 \
+       -module_name combi_mult_18x18 -dir ../IP
 
-create_ip -name mult_gen -vendor xilinx.com -library ip -version 12.0 -module_name combi_mult_18x18 -dir ../.srcs/sources_1/ip
-set_property -dict [list CONFIG.Component_Name {combi_mult_18x18} CONFIG.Multiplier_Construction {Use_Mults} CONFIG.OptGoal {Speed} CONFIG.PipeStages {0}] [get_ips combi_mult_18x18]
-generate_target {instantiation_template} [get_files ../.srcs/sources_1/ip/combi_mult_18x18/combi_mult_18x18.xci]
-set_property generate_synth_checkpoint false [get_files  ../.srcs/sources_1/ip/combi_mult_18x18/combi_mult_18x18.xci]
-generate_target all [get_files  ../.srcs/sources_1/ip/combi_mult_18x18/combi_mult_18x18.xci]
-export_ip_user_files -of_objects [get_files ../.srcs/sources_1/ip/combi_mult_18x18/combi_mult_18x18.xci] -no_script -sync -force -quiet
-export_simulation -of_objects [get_files ../.srcs/sources_1/ip/combi_mult_18x18/combi_mult_18x18.xci] -directory .ip_user_files/sim_scripts -ip_user_files_dir .ip_user_files -ipstatic_source_dir .ip_user_files/ipstatic -lib_map_path [list {modelsim=./.cache/compile_simlib/modelsim} {questa=./.cache/compile_simlib/questa} {riviera=./.cache/compile_simlib/riviera} {activehdl=./.cache/compile_simlib/activehdl}] -use_ip_compiled_libs -force -quiet
+       set_property -dict [list \
+           CONFIG.Multiplier_Construction {Use_Mults} CONFIG.OptGoal {Speed} \
+           CONFIG.PipeStages {0}] [get_ips combi_mult_18x18]
+       generate_target {instantiation_template} [get_files ../IP/combi_mult_18x18/combi_mult_18x18.xci]
 
+       set_property generate_synth_checkpoint false [get_files  ../IP/combi_mult_18x18/combi_mult_18x18.xci]
+
+       #generate_target all [get_files  ../IP/combi_mult_18x18/combi_mult_18x18.xci]
+
+       export_ip_user_files \
+           -of_objects [get_files ../IP/combi_mult_18x18/combi_mult_18x18.xci] \
+           -no_script -sync -force -quiet
+
+       export_simulation -of_objects [get_files ../IP/combi_mult_18x18/combi_mult_18x18.xci] \
+           -directory ../IP/.ip_user_files/sim_scripts \
+           -ip_user_files_dir ../IP/.ip_user_files \
+           -ipstatic_source_dir ../IP/.ip_user_files/ipstatic \
+           -lib_map_path [list {modelsim=./.cache/compile_simlib/modelsim} \
+               {questa=./.cache/compile_simlib/questa} \
+               {riviera=./.cache/compile_simlib/riviera} \
+               {activehdl=./.cache/compile_simlib/activehdl}] \
+           -use_ip_compiled_libs -force -quiet
+
+       generate_target all [get_ips]
+
+#        # Synthesize all the IP
+#        synth_ip [get_ips]
+}
