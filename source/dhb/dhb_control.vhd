99b5f87f9b585fe936154d83375a87956cbf8a13
Jari Honkanen <jari.honkanen@lut.fi>
2019-07-04 14:16:06 +0300

    add pi control to dhb contorl

--- source/dhb/dhb_control.vhd
+++ source/dhb/dhb_control.vhd
@@ -36,11 +36,43 @@ end dhb_control;
 
 architecture rtl of dhb_control is
 
+component seq_pi_control is 
+	generic(
+				gen_pi_sat_high : integer; 
+				gen_pi_sat_low : integer;
+				gen_left_shift_p_gain : integer;
+				gen_offset_sign18 : integer
+			);
+    port(
+	    pi_clk : in std_logic;
+        si_rstn : in std_logic;
+	    
+	    si_start : in std_logic;
+	    so_pi_busy : out std_logic;
+	    so_pi_out_rdy : out std_logic;
+
+	    so_sign18_pi_out : out signed(17 downto 0);
+
+	    si_sign18_ref : in signed(17 downto 0);
+	    si_sign18_meas : in signed(17 downto 0);
+
+	    si_sign18_p_gain : in signed(17 downto 0);
+	    si_sign18_i_gain : in signed(17 downto 0)
+	);
+end component;
     
 	signal jihuu : rec_dhb_input;
+    signal r_so_sign18_pi_out : signed(17 downto 0);
+    signal r_si_sign18_meas : signed(17 downto 0);
+    signal voltage_ctrl_rdy : std_logic;
 
 begin
 
+dhb_voltage_control : seq_pi_control
+	generic map(1700,948,0,0)
+port map(core_clk, si_rstn, dhb_adc_control.ad_rdy_trigger,open, voltage_ctrl_rdy, r_so_sign18_pi_out, 18d"13945", r_si_sign18_meas, 18d"1500", 18d"500");
+
+r_si_sign18_meas <= resize(signed(dhb_adc_control.std16_ad_bus),18);
 jihuu.u12_dhb_half_period <= 12d"472";
 
 dhb_modulator : phase_modulator
