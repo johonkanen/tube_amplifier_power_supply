5673e6c97afed93ee133dc41001271437a27d2d6
Jari Honkanen <jari.honkanen@lut.fi>
2019-07-05 11:56:37 +0300

    move spi simulation to own folder

--- source/ext_ad/simulation/sim_spi3w_ads7056_driver_tb.bat
+++ source/ext_ad/simulation/sim_spi3w_ads7056_driver_tb.bat
@@ -1,4 +1,4 @@
-ghdl -a --std=08 --ieee=synopsys spi3w_ads7056_driver.vhd
+ghdl -a --std=08 --ieee=synopsys ..\spi3w_ads7056_driver.vhd
 ghdl -a --std=08 --ieee=synopsys spi3w_ads7056_driver_tb.vhd
 ghdl -e --std=08 --ieee=synopsys spi3w_ads7056_driver_tb
 ghdl -r --std=08 --ieee=synopsys spi3w_ads7056_driver_tb --vcd=spi3w_ads7056_driver_tb.vcd
--- source/ext_ad/simulation/sim_spi3w_max11115.bat
+++ source/ext_ad/simulation/sim_spi3w_max11115.bat
@@ -1,4 +1,7 @@
-ghdl -a --std=08 --ieee=synopsys ext_ad_spi3w.vhd
+DEL *.vcd
+DEL *.o
+DEL *.cf
+ghdl -a --std=08 --ieee=synopsys ..\ext_ad_spi3w.vhd
 ghdl -a --std=08 --ieee=synopsys spi3w_testbench.vhd
 ghdl -e --std=08 --ieee=synopsys spi3w_testbench
 ghdl -r --std=08 --ieee=synopsys spi3w_testbench --vcd=spi3w_testbench.vcd
