8ad59fe19f6b3f765c0b111fe79c98f14a07875e
Jari Honkanen <jari.honkanen@lut.fi>
2019-07-18 09:42:01 +0300

    add procedure to start uart in data control

--- source/system_control/data_control.vhd
+++ source/system_control/data_control.vhd
@@ -200,6 +200,22 @@ signal dhb_adc_control : rec_ext_ad_ctrl;
 signal std18_test_data : std_logic_vector(17 downto 0);
 signal test_data_rdy : std_logic;
 
+procedure send_data_to_uart
+(
+    signal std3_ad_ch : in std_logic_vector(2 downto 0);
+    signal ad_control : in rec_onboard_ad_ctrl_signals;
+    signal uart_start_event : out std_logic;
+    signal uart_data : out std_logic_vector(15 downto 0)
+) is
+begin
+    if ad_control.std3_ad_address = 3d"3" AND ad_control.ad_rdy_trigger = '1' then
+        uart_start_event <= '1';
+        uart_data <= ad_control.std16_ad_bus;
+    else
+        uart_start_event <= '0';
+    end if;
+end send_data_to_uart;
+
 begin
 
 uart : uart_event_ctrl
