a72e2efacddf1be9e147f259bb1f608d8943291d
Jari Honkanen <jari.honkanen@lut.fi>
2019-09-28 16:13:51 +0300

    start implementing sine and cosine funcitions

--- source/dsp/sin_cos16bit.vhd
+++ source/dsp/sin_cos16bit.vhd
@@ -5,50 +5,50 @@ use IEEE.numeric_std.ALL;
 library work;
     use work.alu16bit_pkg.all;
 
-entity rad4_inv16bit is
+entity sin_cos16bit is
     port (
         core_clk : in std_logic;
         rstn : in std_logic;
 
-        si_start_div : in std_logic;
+        si_start_sin_cos : in std_logic;
         data_to_invert : in signed(17 downto 0);
 
         si36_mpy1_result : in std_logic_vector(35 downto 0); 
         si36_mpy2_result : in std_logic_vector(35 downto 0); 
         si_mult_rdy : in std_logic;
 
-        so18_div_mpy1_a : out std_logic_vector(17 downto 0); 
-        so18_div_mpy1_b : out std_logic_vector(17 downto 0); 
+        so18_sincos_mpy1_a : out std_logic_vector(17 downto 0); 
+        so18_sincos_mpy1_b : out std_logic_vector(17 downto 0); 
 
-        so18_div_mpy2_a : out std_logic_vector(17 downto 0); 
-        so18_div_mpy2_b : out std_logic_vector(17 downto 0); 
+        so18_sincos_mpy2_a : out std_logic_vector(17 downto 0); 
+        so18_sincos_mpy2_b : out std_logic_vector(17 downto 0); 
 
-        so_div_start_mpy : out std_logic; 
+        so_sincos_start_mpy : out std_logic; 
 
-        so_div_busy : out std_logic;
-        so_div_rdy : out std_logic;
-        so18_div_out : out std_logic_vector(17 downto 0)
+        so_sincos_busy : out std_logic;
+        so_sincos_rdy : out std_logic;
+        so18_sincos_out : out std_logic_vector(17 downto 0)
     );
-end entity rad4_inv16bit;
+end entity sin_cos16bit;
 
-architecture rtl of rad4_inv16bit is
+architecture rtl of sin_cos16bit is
 
-    constant inv_magic_number : std_logic_vector(17 downto 0) := 18d"21750";
-    -- constants optimized for max error of 10 units for 10bit sqrt using multiplicative normalized method
-    type u15_array is array (0 to 2) of unsigned(17 downto 0);
-    constant sin_chebychev : u15_array := (18d"26695", 18d"10579", 18d"1272");
-    constant cos_chebyshev : u15_array := (18d"32767", 18d"20201", 18d"4029");
-    type sin_chebyshev16bit is record
-        s1 : unsigned(17 downto 0);
-        s3 : unsigned(17 downto 0);
-        s5 : unsigned(17 downto 0);
+    subtype int18 is integer range -2**17 to 2**17-1;
+-- sin constants s1 51472 s3 84635 s5 40720
+-- cos constants c0 16384 c2 80805 c4 64473
+    type sin_chebyshev18bit is record
+        s1 : int18;
+        s3 : int18;
+        s5 : int18;    
     end record;
+    constant sin_factors : sin_chebyshev18bit := (s1 => 51472, s3=> 84635, s5=> 40720);
 
-    type cos_chebyshev16bit is record
-        s0 : unsigned(17 downto 0);
-        s2 : unsigned(17 downto 0);
-        s4 : unsigned(17 downto 0);
+    type cos_chebyshev18bit is record
+        c0 : int18;
+        c2 : int18;
+        c4 : int18;    
     end record;
+    constant cos_factors : cos_chebyshev18bit := (c0 => 16384, c2=> 80805, c4=> 64473);
 
     function rounded_mpy
     (
@@ -67,98 +67,97 @@ architecture rtl of rad4_inv16bit is
 begin
 
 div : process(core_clk)
-    type t_division_states is (idle,m1,m2,m3, m4,rdy);
-    variable st_division_states : t_division_states;
+    type t_sincos_states is (idle,m1,m2,m3, m4,rdy);
+    variable st_sincos_states : t_sincos_states;
 begin
     if rising_edge(core_clk) then
         if rstn = '0' then
         -- reset state
-            st_division_states := idle;
-            so_div_rdy <= '0';
-            so_div_busy <= '0';
-            so18_div_out <= (others => '0');
-            so_div_start_mpy <= '0';
-            so18_div_mpy1_a <= (others => '0');
-            so18_div_mpy1_b <= (others => '0');
-            so18_div_mpy2_a <= (others => '0');
-            so18_div_mpy2_b <= (others => '0');
+            st_sincos_states := idle;
+            so_sincos_rdy <= '0';
+            so_sincos_busy <= '0';
+            so18_sincos_out <= (others => '0');
+            so_sincos_start_mpy <= '0';
+            so18_sincos_mpy1_a <= (others => '0');
+            so18_sincos_mpy1_b <= (others => '0');
+            so18_sincos_mpy2_a <= (others => '0');
+            so18_sincos_mpy2_b <= (others => '0');
         else
-            CASE st_division_states is 
+            CASE st_sincos_states is 
                 WHEN idle =>
-                    so_div_rdy <= '0';
-                    so18_div_out <= (others => '0');
-                    -- invert data_to_invert
+                    so_sincos_rdy <= '0';
+                    so18_sincos_out <= (others => '0');
 
-                    so18_div_mpy2_a <= (others => '0');
-                    so18_div_mpy2_b <= (others => '0');
-                    if si_start_div = '1' then
-                        st_division_states := m1;
-                        so18_div_mpy1_a <= inv_magic_number; -- magic constant, tested to be optimal
-                        so18_div_mpy1_b <= std_logic_vector(data_to_invert);
-                        so_div_start_mpy <= '1';
+                    so18_sincos_mpy2_a <= (others => '0');
+                    so18_sincos_mpy2_b <= (others => '0');
+                    if si_start_sin_cos = '1' then
+                        st_sincos_states := m1;
+                        --so18_sincos_mpy1_a <= inv_magic_number; -- magic constant, tested to be optimal
+                        so18_sincos_mpy1_b <= std_logic_vector(data_to_invert);
+                        so_sincos_start_mpy <= '1';
                     else
-                        st_division_states := idle;
-                        so18_div_mpy1_a <= (others => '0');
-                        so18_div_mpy1_b <= (others => '0');
-                        so_div_start_mpy <= '0';
+                        st_sincos_states := idle;
+                        so18_sincos_mpy1_a <= (others => '0');
+                        so18_sincos_mpy1_b <= (others => '0');
+                        so_sincos_start_mpy <= '0';
                     end if;
                 WHEN m1 =>
-                    so_div_rdy <= '0';
-                    so18_div_out <= (others => '0');
+                    so_sincos_rdy <= '0';
+                    so18_sincos_out <= (others => '0');
                     if si_mult_rdy = '1' then
-                        so18_div_mpy1_a <= "00"&rounded_mpy(si36_mpy1_result);
-                        so18_div_mpy1_b <= "00"& not (rounded_mpy(si36_mpy1_result));
+                        so18_sincos_mpy1_a <= "00"&rounded_mpy(si36_mpy1_result);
+                        so18_sincos_mpy1_b <= "00"& not (rounded_mpy(si36_mpy1_result));
 
-                        so18_div_mpy2_a <= "00"&(not rounded_mpy(si36_mpy1_result));
-                        so18_div_mpy2_b <= inv_magic_number;
-                        so_div_start_mpy <= '1';
-                        st_division_states := m2;
+                        so18_sincos_mpy2_a <= "00"&(not rounded_mpy(si36_mpy1_result));
+                        --so18_sincos_mpy2_b <= inv_magic_number;
+                        so_sincos_start_mpy <= '1';
+                        st_sincos_states := m2;
                     else
-                        so_div_start_mpy <= '0';
-                        st_division_states := m1;
+                        so_sincos_start_mpy <= '0';
+                        st_sincos_states := m1;
                     end if;
                 WHEN m2 =>
-                    so_div_rdy <= '0';
-                    so18_div_out <= (others => '0');
+                    so_sincos_rdy <= '0';
+                    so18_sincos_out <= (others => '0');
                     if si_mult_rdy = '1' then
-                        so18_div_mpy1_a <= "00"&rounded_mpy(si36_mpy1_result);
-                        so18_div_mpy1_b <= "00"&(not rounded_mpy(si36_mpy1_result));
+                        so18_sincos_mpy1_a <= "00"&rounded_mpy(si36_mpy1_result);
+                        so18_sincos_mpy1_b <= "00"&(not rounded_mpy(si36_mpy1_result));
 
-                        so18_div_mpy2_a <= "00"&(not rounded_mpy(si36_mpy1_result));
-                        so18_div_mpy2_b <= "00"&rounded_mpy(si36_mpy2_result);
-                        st_division_states := m3;
-                        so_div_start_mpy <= '1';
+                        so18_sincos_mpy2_a <= "00"&(not rounded_mpy(si36_mpy1_result));
+                        so18_sincos_mpy2_b <= "00"&rounded_mpy(si36_mpy2_result);
+                        st_sincos_states := m3;
+                        so_sincos_start_mpy <= '1';
                     else
-                        st_division_states := m2;
-                        so_div_start_mpy <= '0';
+                        st_sincos_states := m2;
+                        so_sincos_start_mpy <= '0';
                     end if;
                 WHEN m3 =>
-                    so_div_rdy <= '0';
-                    so18_div_out <= (others => '0');
+                    so_sincos_rdy <= '0';
+                    so18_sincos_out <= (others => '0');
                     if si_mult_rdy = '1' then
-                        so18_div_mpy1_a <= "00"&rounded_mpy(si36_mpy1_result);
-                        so18_div_mpy1_b <= "00"&(not rounded_mpy(si36_mpy1_result));
+                        so18_sincos_mpy1_a <= "00"&rounded_mpy(si36_mpy1_result);
+                        so18_sincos_mpy1_b <= "00"&(not rounded_mpy(si36_mpy1_result));
 
-                        so18_div_mpy2_a <= "00"&(not rounded_mpy(si36_mpy1_result));
-                        so18_div_mpy2_b <= "00"&rounded_mpy(si36_mpy2_result);
-                        st_division_states := rdy;
-                        so_div_start_mpy <= '1';
+                        so18_sincos_mpy2_a <= "00"&(not rounded_mpy(si36_mpy1_result));
+                        so18_sincos_mpy2_b <= "00"&rounded_mpy(si36_mpy2_result);
+                        st_sincos_states := rdy;
+                        so_sincos_start_mpy <= '1';
                     else
-                        st_division_states := m3;
-                        so_div_start_mpy <= '0';
+                        st_sincos_states := m3;
+                        so_sincos_start_mpy <= '0';
                     end if;
                 WHEN rdy =>
-                    so_div_start_mpy <= '0';
+                    so_sincos_start_mpy <= '0';
                     if si_mult_rdy = '1' then
-                        so18_div_out <= "00"&rounded_mpy(si36_mpy2_result);
-                        so_div_rdy <= '1';
-                        st_division_states := idle;
+                        so18_sincos_out <= "00"&rounded_mpy(si36_mpy2_result);
+                        so_sincos_rdy <= '1';
+                        st_sincos_states := idle;
                     else
-                        st_division_states := rdy;
-                        so_div_rdy <= '0';
+                        st_sincos_states := rdy;
+                        so_sincos_rdy <= '0';
                     end if;
                 WHEN others => 
-                    -- do nothing
+                    st_sincos_states := idle;
             end CASE;
         end if; -- rstn
     end if; --rising_edge
