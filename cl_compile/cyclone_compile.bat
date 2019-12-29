f63d598bb67baf2ea38a457f218b0219ccbb0e81
Jari Honkanen <jari.honkanen@lut.fi>
2019-12-29 14:18:48 +0200

    add cyclone compile bat file

---
+++ cl_compile/cyclone_compile.bat
@@ -0,0 +1,5 @@
+quartus_sh -t ../cyclone_10/tcl/cl10_tube_psu_v5.tcl -source_path ../cyclone_10/tcl/
+
+rem quartus_pgm -c "USB-Blaster [USB-0]" -m JTAG -o "p;./output_files/cl10_tubepsu.sof"
+quartus_cpf -c ../cyclone_10/tcl/generate_flash_image.cof
+quartus_pgm -i -c "USB-Blaster [USB-0]" -m JTAG -o "ipv;./output_files/output_file.jic"
