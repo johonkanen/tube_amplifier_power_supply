2c2ca7b20c1eda5c59821f1b1ab5ba95efc99621
Jari kotikone <jari.honkanen@lut.fi>
2020-01-14 22:30:55 +0200

    add vendor specifics pkg

---
+++ source/ecp5/vendor_specifics_pkg.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+package vendor_specifics_pkg is
+
+
+constant g_vendor_specific_uart_clks_per_bit : integer := 24;
+constant g_vendor_specific_RX_bytes_in_word : integer := 2;
+constant g_vendor_specific_TX_bytes_in_word : integer := 2;
+
+end vendor_specifics_pkg;
