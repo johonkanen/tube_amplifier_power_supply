22408c6a53d81211a93c1a5a329c5d1a10f8e45c
Jari kotikone <jari.honkanen@lut.fi>
2020-02-02 00:38:36 +0200

    compile onboard ad testbench

--- source/top/system_control/component_interconnect/onboard_ad_control/simulation/sim_onboard_adc.bat
+++ source/top/system_control/component_interconnect/onboard_ad_control/simulation/sim_onboard_adc.bat
@@ -1,8 +1,9 @@
 rem simulate onboard_adc.vhd
+ghdl -a --ieee=synopsys --work=onboard_adc_library ..\..\ext_ad\ext_ad_spi3w.vhd
+ghdl -a --ieee=synopsys --work=onboard_adc_library ..\..\..\..\..\cyclone_10lp\cl10_adc_wrapper.vhd
 ghdl -a --ieee=synopsys --work=onboard_adc_library ../../../../..//top/system_control/component_interconnect/onboard_ad_control/onboard_ad_control_pkg.vhd
 ghdl -a --ieee=synopsys --work=onboard_adc_library ../../../../..//top/system_control/component_interconnect/onboard_ad_control/onboard_ad_control_internal_pkg.vhd
-ghdl -a --ieee=synopsys ..\..\..\..\..\cyclone_10lp\cl10_adc_wrapper.vhd
-ghdl -a --ieee=synopsys ..\..\ext_ad\ext_ad_spi3w.vhd
+ghdl -a --ieee=synopsys --work=onboard_adc_library ../../../../..//top/system_control/component_interconnect/onboard_ad_control/onboard_ad_control.vhd
 ghdl -a --ieee=synopsys tb_onboard_adc.vhd
 ghdl -e --ieee=synopsys tb_onboard_adc
 ghdl -r --ieee=synopsys tb_onboard_adc --vcd=tb_onboard_adc.vcd
--- source/top/system_control/component_interconnect/onboard_ad_control/simulation/tb_onboard_adc.vhd
+++ source/top/system_control/component_interconnect/onboard_ad_control/simulation/tb_onboard_adc.vhd
@@ -21,6 +21,11 @@ architecture sim of tb_onboard_adc is
     constant clock_half_per : time := 0.5 ns;
     constant simtime_in_clocks : integer := 50;
 
+    signal onboard_ad_control_clocks   : onboard_ad_control_clock_group;
+    signal onboard_ad_control_FPGA_in  : onboard_ad_control_FPGA_input_group;
+    signal onboard_ad_control_FPGA_out : onboard_ad_control_FPGA_output_group;
+    signal onboard_ad_control_data_in  : onboard_ad_control_data_input_group;
+    signal onboard_ad_control_data_out : onboard_ad_control_data_output_group;
 
 begin
 
@@ -46,5 +51,54 @@ begin
             end loop;
         wait;
     end process;
+------------------------------------------------------------------------
+    test_adc : process(simulator_clock)
+        variable adc_test_counter : integer;
+    begin
+        if rising_edge(simulator_clock) then
+            if rstn = '0' then
+            -- reset state
+                adc_test_counter := 0;
+                onboard_ad_control_data_in <=(false, 0, false, 0);
+            else
+                adc_test_counter := adc_test_counter + 1;
+                if adc_test_counter = 896 then
+                    adc_test_counter := 0;
+                end if;
+
+                onboard_ad_control_data_in.ada_start_request <= false;
+                onboard_ad_control_data_in.adb_start_request <= false;
+                CASE adc_test_counter is
+                    WHEN 0 =>
+                        onboard_ad_control_data_in <= trigger_adc(1);
+                    WHEN 128 =>
+                        onboard_ad_control_data_in <= trigger_adc(2);
+                    WHEN 256 =>
+                        onboard_ad_control_data_in <= trigger_adc(3);
+                    WHEN 384 =>
+                        onboard_ad_control_data_in <= trigger_adc(4);
+                    WHEN 512 =>
+                        onboard_ad_control_data_in <= trigger_adc(5);
+                    WHEN 640 =>
+                        onboard_ad_control_data_in <= trigger_adc(6);
+                    WHEN 768 =>
+                        onboard_ad_control_data_in <= trigger_adc(0);
+                    WHEN others =>
+                end CASE;
+            end if; -- rstn
+        end if; --rising_edge
+    end process test_adc;	
+------------------------------------------------------------------------
+
+    onboard_ad_control_clocks <= (simulator_clock, simulator_clock, rstn);
+    u_onboard_ad_control : onboard_ad_control 
+    port map(
+        onboard_ad_control_clocks,   
+        onboard_ad_control_FPGA_in,  
+        onboard_ad_control_FPGA_out, 
+        onboard_ad_control_data_in,
+        onboard_ad_control_data_out 
+    );
+------------------------------------------------------------------------
 
 end sim;
