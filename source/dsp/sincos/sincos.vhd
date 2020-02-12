1c40ba9c62fc8d15302d2e81eda5784a9af6b511
Jari kotikone <jari.honkanen@lut.fi>
2020-02-12 20:00:46 +0200

    start refactoring sincos to own package

--- source/dsp/sincos/sincos.vhd
+++ source/dsp/sincos/sincos.vhd
@@ -1,30 +1,22 @@
-LIBRARY ieee  ; 
-LIBRARY std  ; 
-    USE ieee.NUMERIC_STD.all  ; 
-    USE ieee.std_logic_1164.all  ; 
-    USE ieee.std_logic_textio.all  ; 
-    use ieee.math_real.all;
-    USE std.textio.all  ; 
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
 
 library work;
     use work.multiplier_pkg.all;
 
-entity tb_sincos is
-end;
+entity sincos is
+    port (
+        clk : in std_logic	
+    );
+end entity sincos;
 
-architecture sim of tb_sincos is
-    signal rstn : std_logic;
 
-    signal simulation_running : boolean;
-    signal simulator_clock : std_logic;
-    constant clock_per : time := 1 ns;
-    constant clock_half_per : time := 0.5 ns;
-    constant simtime_in_clocks : integer := 20e3;
+architecture rtl of sincos is
 
     signal multiplier_clocks   : multiplier_clock_group;
     signal multiplier_data_in  : multiplier_data_input_group;
     signal multiplier_data_out : multiplier_data_output_group;
-
 --------------- simulation signals -------------------------------------
     signal mpy_test : sign36;
     signal testcounter : integer;
@@ -53,30 +45,8 @@ architecture sim of tb_sincos is
         return to_integer((sign16_angle(13 downto 0)));
     end reduce_angle;
 
+
 begin
-
-    simtime : process
-    begin
-        simulation_running <= true;
-        wait for simtime_in_clocks*clock_per;
-        simulation_running <= false;
-        wait;
-    end process simtime;	
-
-
-    sim_clock_gen : process
-    begin
-        simulator_clock <= '0';
-        rstn <= '0';
-        simulator_clock <= '0';
-        wait for clock_half_per;
-        while simulation_running loop
-            wait for clock_half_per;
-                rstn <= '1';
-                simulator_clock <= not simulator_clock;
-            end loop;
-        wait;
-    end process;
 
     multiplier_clocks.dsp_clock <= simulator_clock;
     u_multiplier : multiplier
@@ -203,4 +173,6 @@ begin
         end if; -- rstn
     end process calculate_sincos;	
 
-end sim;
+
+
+end rtl;
---
+++ source/dsp/sincos/sincos_pkg.vhd
@@ -0,0 +1,28 @@
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+package sincos_pkg is
+
+    type sincos_clock_group is record
+        clock : std_logic;
+    end record;
+    
+    type sincos_data_input_group is record
+        angle_pirad : integer range 0 to 2**16;
+    end record;
+    
+    type sincos_data_output_group is record
+        sincos_is_ready : boolean;
+    end record;
+    
+    component sincos is
+        port (
+            sincos_clocks : in sincos_clock_group;
+            sincos_data_in : in sincos_data_input_group;
+            sincos_data_out : out sincos_data_output_group
+        );
+    end component sincos;
+        
+
+end package sincos_pkg;
