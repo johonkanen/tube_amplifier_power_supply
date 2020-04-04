ec71e319f6611e62375b643caac5cd0a14fb6868
Jari kotikone <jari.honkanen@lut.fi>
2020-04-04 15:12:59 +0300

    add deadtime moudules for primary and secondary

--- source/top/system_control/component_interconnect/power_supply_control/dhb_control/phase_modulator/deadtime_pkg.vhd
+++ source/top/system_control/component_interconnect/power_supply_control/dhb_control/phase_modulator/deadtime_pkg.vhd
@@ -20,6 +20,9 @@ package deadtime_pkg is
         deadtime_cycles : uint12;
     end record;
 
+    type deadtime_FPGA_output_array is array (integer range <>) of deadtime_FPGA_output_group;
+    type deadtime_data_input_array is array (integer range <>) of deadtime_data_input_group;
+
     component deadtime is
         port (
             deadtime_clocks : in deadtime_clock_group; 
@@ -28,9 +31,10 @@ package deadtime_pkg is
         );
     end component deadtime;
     
+    -- constant number_of_half_bridge_modules : integer := 2;
     -- signal deadtime_clocks   : deadtime_clock_group;
-    -- signal deadtime_FPGA_out : deadtime_FPGA_output_group;
-    -- signal deadtime_data_in  : deadtime_data_input_group;
+    -- signal deadtime_FPGA_out : deadtime_FPGA_output_array(1 to number_of_half_bridge_modules); 
+    -- signal deadtime_data_in : deadtime_data_input_array(1 to number_of_half_bridge_modules);
     
     -- u_deadtime : deadtime
     -- port map( deadtime_clocks,
