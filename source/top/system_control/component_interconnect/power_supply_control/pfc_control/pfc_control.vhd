0c3c420a81296a048d7b5b0d937e3a028337d20b
jari <jaarhonka@yahoo.com>
2020-04-13 12:00:47 +0300

    add delay for pfc control

--- source/top/system_control/component_interconnect/power_supply_control/pfc_control/pfc_control.vhd
+++ source/top/system_control/component_interconnect/power_supply_control/pfc_control/pfc_control.vhd
@@ -2,13 +2,16 @@ library ieee;
     use ieee.std_logic_1164.all;
     use ieee.numeric_std.all;
 
+library onboard_adc_library;
+    use onboard_adc_library.psu_measurement_interface_pkg.all;
+
+library common_library;
+    use common_library.timing_pkg.all;
+
 library work;
     use work.pfc_control_pkg.all;
     use work.pfc_modulator_pkg.all;
     use work.multiplier_pkg.all;
-
-library onboard_adc_library;
-    use onboard_adc_library.psu_measurement_interface_pkg.all;
 
 entity pfc_control is
         generic( g_carrier_max_value : integer);
@@ -21,21 +24,39 @@ entity pfc_control is
 end pfc_control;
 
 architecture rtl of pfc_control is
-
+------------------------------------------------------------------------
     alias core_clock      is pfc_control_clocks.core_clock;
     alias modulator_clock is pfc_control_clocks.modulator_clock;
     alias pll_lock        is pfc_control_clocks.pll_lock;
-
+------------------------------------------------------------------------
     signal multiplier_clocks   : multiplier_clock_group;
     signal multiplier_data_in  : multiplier_data_input_group;
     signal multiplier_data_out : multiplier_data_output_group;
-
+------------------------------------------------------------------------
+    signal delay_timer_data_in  : delay_timer_data_input_group;
+    signal delay_timer_data_out : delay_timer_data_output_group;
+------------------------------------------------------------------------
     signal pfc_modulator_clocks   : pfc_modulator_clock_group;
     signal pfc_modulator_data_in  : pfc_modulator_data_input_group;
     signal pfc_modulator_data_out : pfc_modulator_data_output_group;
+------------------------------------------------------------------------
 
 begin
 
+------------------------------------------------------------------------
+    delay_50us : delay_timer
+    generic map (count_up_to => 5*1280)
+    port map( core_clock,
+    	  delay_timer_data_in,
+    	  delay_timer_data_out);
+------------------------------------------------------------------------
+    multiplier_clocks.dsp_clock <= core_clock;
+    u_multiplier : multiplier
+        port map(
+            multiplier_clocks, 
+            multiplier_data_in,
+            multiplier_data_out 
+        );
 ------------------------------------------------------------------------
     pfc_control : process(core_clock)
         type t_pfc_control_state is (idle, precharge, pfc_running);
@@ -62,14 +83,6 @@ begin
             end if; -- rstn
         end if; --rising_edge
     end process pfc_control;	
-------------------------------------------------------------------------
-    multiplier_clocks.dsp_clock <= core_clock;
-    u_multiplier : multiplier
-        port map(
-            multiplier_clocks, 
-            multiplier_data_in,
-            multiplier_data_out 
-        );
 ------------------------------------------------------------------------
     pfc_modulator_clocks <= (modulator_clock => modulator_clock, core_clock => core_clock);
     pfc_modulator_data_in.pfc_carrier <= pfc_control_data_in.pfc_carrier;
