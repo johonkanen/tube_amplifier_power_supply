1faed2e1d0a26b4aa690b32ed40d9060c710b065
jari <jaarhonka@yahoo.com>
2020-05-11 22:27:35 +0300

    start adding full pfc control

--- source/top/system_control/component_interconnect/power_supply_control/pfc_control/pfc_control.vhd
+++ source/top/system_control/component_interconnect/power_supply_control/pfc_control/pfc_control.vhd
@@ -11,8 +11,8 @@ library common_library;
 
 library work;
     use work.pfc_control_pkg.all;
-    use work.pfc_modulator_pkg.all;
     use work.multiplier_pkg.all;
+    use work.pfc_modulator_pkg.all;
 
 entity pfc_control is
         generic( g_carrier_max_value : integer);
@@ -35,6 +35,10 @@ architecture rtl of pfc_control is
     signal multiplier_data_in  : multiplier_data_input_group;
     signal multiplier_data_out : multiplier_data_output_group;
 ------------------------------------------------------------------------
+    signal multiplier_2_clocks   : multiplier_clock_group;
+    signal multiplier_2_data_in  : multiplier_data_input_group;
+    signal multiplier_2_data_out : multiplier_data_output_group;
+------------------------------------------------------------------------
     signal delay_timer_data_in  : delay_timer_data_input_group;
     signal delay_timer_data_out : delay_timer_data_output_group;
 ------------------------------------------------------------------------
@@ -48,6 +52,14 @@ architecture rtl of pfc_control is
     signal DC_link_voltage_measurement : int18;
     signal AC_voltage_measurement : int18;
     signal pfc_current_is_buffered : boolean;
+    signal vac_is_buffered : boolean;
+
+    constant dc_link_ref_150V : int18 := 32768/663*150;
+    constant dc_link_ref_200V : int18 := 32768/663*200;
+    constant dc_link_ref_250V : int18 := 32768/663*250;
+    constant dc_link_ref_300V : int18 := 32768/663*300;
+    constant dc_link_ref_350V : int18 := 32768/663*350;
+    constant dc_link_ref_400V : int18 := 32768/663*400;
 
 begin
 
@@ -66,10 +78,19 @@ begin
             multiplier_data_out 
         );
 ------------------------------------------------------------------------
+    multiplier_clocks.dsp_clock <= core_clock;
+    u_multiplier_2 : multiplier
+        port map(
+            multiplier_2_clocks, 
+            multiplier_2_data_in,
+            multiplier_2_data_out 
+        );
+------------------------------------------------------------------------
     pfc_control : process(core_clock)
         type t_pfc_control_state is (idle, precharge, rampup, pfc_running);
         variable st_pfc_control_state : t_pfc_control_state;
-        variable process_counter : uint16;
+        variable process_counter : uint8;
+        variable voltage_process_counter : uint8;
 
         variable err : int18;
         variable pi_out : int18;
@@ -78,9 +99,15 @@ begin
         constant ikp : int18 := 22e3;
         constant iki : int18 := 250;
 
-        constant vkp : int18 := 22e3;
-        constant vki : int18 := 250;
+        constant vkp : int18 := 1500;
+        constant vki : int18 := 50;
+
+        variable voltage_err : int18;
+        variable voltage_pi_out : int18;
+        variable voltage_integrator : int18;
+
         constant radix_15 : int18 := 15;
+
     begin
         if rising_edge(core_clock) then
             if pll_lock = '0' then
@@ -88,9 +115,17 @@ begin
                 st_pfc_control_state := idle;
                 set_duty(0,pfc_modulator_data_in);
                 disable_pfc_modulator(pfc_modulator_data_in);
+                voltage_process_counter := 0;
+                process_counter := 0;
+                voltage_integrator := 0;
+
+                voltage_err        := 0;
+                voltage_pi_out     := 0;
+                voltage_integrator := 0;
             else
                 ------------- buffer pfc measurements --------------
                 pfc_current_is_buffered <= pfc_I1_is_ready(measurement_interface);
+                vac_is_buffered <= vac_is_ready(measurement_interface);
 
                 get_pfc_I1 (measurement_interface,pfc_I1_measurement);
                 get_pfc_I2 (measurement_interface,pfc_I2_measurement);
@@ -124,7 +159,60 @@ begin
 
                     WHEN pfc_running => 
 
-                        -- TODO, refactor PI controller to own component
+                    ----------------- voltage control ----------------         
+                        CASE voltage_process_counter is
+                            WHEN 0 => 
+
+                                if vac_is_buffered then
+                                    increment(voltage_process_counter);
+                                    voltage_err := dc_link_ref_150V - DC_link_voltage_measurement;
+                                end if;
+
+                            WHEN 1 => 
+                                alu_mpy(vkp,voltage_err,multiplier_2_data_in);
+                                increment(voltage_process_counter);
+
+                            WHEN 2 => 
+                                -- pipeline integrator calculation
+                                increment(voltage_process_counter);
+                            WHEN 3 => 
+                                -- pipeline integrator calculation
+                                alu_mpy(vki,voltage_err,multiplier_2_data_in);
+                                increment(voltage_process_counter);
+
+                            WHEN 4 => 
+                                voltage_pi_out := get_result(multiplier_2_data_out,radix_15) + voltage_integrator;
+                                increment(voltage_process_counter);
+
+                            WHEN 5 => 
+                                if voltage_pi_out > 32768 then
+                                    voltage_pi_out := 32768;
+                                    voltage_integrator := 32768-get_result(multiplier_2_data_out,radix_15);
+                                    increment(voltage_process_counter);
+                                end if;
+
+                                if voltage_pi_out < 0 then
+                                    voltage_pi_out := 0;
+                                    voltage_integrator := 0-get_result(multiplier_2_data_out,radix_15);
+                                    increment(voltage_process_counter);
+                                end if;
+                                
+                                if multiplier_is_ready(multiplier_2_data_out) then
+                                    voltage_integrator := voltage_integrator + get_result(multiplier_2_data_out,radix_15);
+                                    increment(voltage_process_counter);
+                                end if;
+
+
+                            WHEN 6 =>
+                                alu_mpy(voltage_pi_out,250,multiplier_2_data_in);
+                                increment(voltage_process_counter);
+
+                            WHEN others => 
+                                voltage_process_counter := 0;
+                        end CASE;
+
+                    ----------------- current control ----------------         
+
                         CASE process_counter is 
                             WHEN 0 =>
                                 if pfc_current_is_buffered then
