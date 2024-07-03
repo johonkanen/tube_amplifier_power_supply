e0f9f777282e03bb5d478ca0851bdac5d46a0872
Jari Honkanen <jaarhonka@yahoo.com>
2024-07-03 21:44:42 +0300

    add efinix titanium build

---
+++ source/float_configuration/artix7_float_configuration_pkg.vhd
@@ -0,0 +1,21 @@
+-- artix 7
+package denormalizer_pipeline_pkg is
+
+    constant pipeline_configuration : natural := 2;
+
+end package denormalizer_pipeline_pkg;
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+package normalizer_pipeline_pkg is
+
+    constant normalizer_pipeline_configuration : natural := 2;
+
+end package normalizer_pipeline_pkg;
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+package float_word_length_pkg is
+
+    constant mantissa_bits : integer := 24;
+    constant exponent_bits : integer := 8;
+
+end package float_word_length_pkg;
