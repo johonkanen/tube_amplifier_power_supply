5fbebf30078cba4bb790dceb6aa826f03f29fe98
Jari Honkanen <jaarhonka@yahoo.com>
2024-07-04 17:14:29 +0300

    move current control to own entity

--- msys2_script_w_plot.sh
+++ msys2_script_w_plot.sh
@@ -6,4 +6,10 @@ export VUNIT_NVC_PATH=/c/Program\ Files/NVC/bin/
 export VUNIT_SIMULATOR=nvc
 cd /c/dev/tube_amplifier_power_supply/
 python vunit_run_tube_amp.py -p 16
-python hil_simulation/python_scripts/boost/plot_rtl_voltage_closed_loop_vs_qspice.py
+
+python hil_simulation/python_scripts/boost/plot_rtl_voltage_closed_loop_vs_qspice.py &
+python hil_simulation/python_scripts/boost/plot_voltage_closed_loop_vs_qspice.py &
+python hil_simulation/python_scripts/boost/plot_boost_closed_loop_vs_qspice.py &
+python hil_simulation/python_scripts/boost/plot_boost_rtl_vs_qspice.py
+
+wait
--- source/efinix_top.vhd
+++ source/efinix_top.vhd
@@ -13,6 +13,93 @@ library ieee;
     use work.half_bridge_current_control_pkg.all;
     use work.voltage_control_pkg.all;
     use work.tubepsu_addresses_pkg;
+
+entity boost_control is
+    port (
+        core_clock            : in std_logic;
+        boost_control_bus_in  : in fpga_interconnect_record;
+        boost_control_bus_out : out fpga_interconnect_record;
+        inductor_current      : in integer range -2**15 to 2**15-1;
+        input_voltage         : in integer range -2**15 to 2**15-1;
+        dc_link_voltage       : in integer range -2**15 to 2**15-1;
+        boost_control_ready   : out boolean;
+        duty_ratio            : out natural range 0 to 2**16-1
+    );
+end entity boost_control;
+
+
+architecture rtl of boost_control is
+
+    signal current_control : current_control_record := init_current_control(16.0, 8.0/4, number_of_fractional_bits => 7);
+    signal self : voltage_control_record := init_voltage_control;
+    signal vkp : integer := to_fixed(0.25     , 15);
+    signal vki : integer := to_fixed(0.016125 , 15);
+
+    signal control_counter       : natural range 0 to 2**15-1      := 0;
+    signal reference_voltage     : integer range -2**15 to 2**15-1 := to_fixed(205.0,7);
+    
+    signal multiplier         : multiplier_record := init_multiplier;
+    signal voltage_multiplier : multiplier_record := init_multiplier;
+    signal divider            : division_record   := init_division;
+    signal divider_multiplier : multiplier_record := init_multiplier;
+
+begin
+    duty_ratio          <= get_int_multiplier_result(multiplier, 7, 20, target_radix => 15);
+    boost_control_ready <= current_control_is_ready(current_control);
+
+    control_procedure : process(core_clock)
+        constant dutymax : integer := to_fixed(0.90, number_of_fractional_bits => 15);
+        constant dutymin : integer := to_fixed(0.10, number_of_fractional_bits => 15);
+        constant vkp     : integer := to_fixed(0.5     , 15);
+        constant vki     : integer := to_fixed(0.016125/4.0 , 15);
+    begin
+        if rising_edge(core_clock) then
+            init_bus(boost_control_bus_out);
+            connect_data_to_address(boost_control_bus_in, boost_control_bus_out, tubepsu_addresses_pkg.reference_voltage_address, reference_voltage);
+
+            create_multiplier(voltage_multiplier);
+            create_voltage_control(self, voltage_multiplier,
+            proportional_gain => vkp,
+            integral_gain     => vki);
+
+            create_divider_and_multiplier(divider,divider_multiplier);
+            create_multiplier(multiplier);
+            create_current_control(current_control,multiplier, divider, divider_multiplier,
+                                    dc_link_voltage ,
+                                    input_voltage   ,
+                                    dutymax         ,
+                                    dutymin);
+
+            if control_counter < 128e6/120e3 then
+                control_counter <= control_counter + 1;
+            else
+                control_counter <= 0;
+                request_current_control(current_control, self.current_ref, inductor_current);
+                request_voltage_control(self, reference_voltage , dc_link_voltage*2);
+            end if;
+
+        end if; --rising_edge
+    end process control_procedure;	
+
+end rtl;
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+    use work.boost_model_pkg.all;
+
+    use work.fpga_interconnect_pkg.all;
+    use work.boost_model_interface_pkg.all;
+
+    use work.real_to_fixed_pkg.all;
+    use work.multiplier_pkg.all;
+    use work.division_pkg.all;
+    use work.half_bridge_current_control_pkg.all;
+    use work.voltage_control_pkg.all;
+    use work.tubepsu_addresses_pkg;
     
 entity efinix_top is
     port (
@@ -26,6 +113,7 @@ architecture rtl of efinix_top is
 
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
+    signal boost_interface       : boost_interface_record;
     signal boost_model_bus : boost_model_interface_record := (others => init_fpga_interconnect);
 
     signal bus_to_communications : fpga_interconnect_record := init_fpga_interconnect;
@@ -37,16 +125,11 @@ architecture rtl of efinix_top is
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
     -- hack signals
-    signal current_control : current_control_record := init_current_control(16.0, 8.0/4, number_of_fractional_bits => 7);
-    signal self : voltage_control_record := init_voltage_control;
-    signal vkp : integer := to_fixed(0.25     , 15);
-    signal vki : integer := to_fixed(0.016125 , 15);
 
-    signal multiplier         : multiplier_record := init_multiplier;
-    signal voltage_multiplier : multiplier_record := init_multiplier;
-    signal divider            : division_record   := init_division;
-    signal divider_multiplier : multiplier_record := init_multiplier;
 
+    /* boost_control_bus_in  : in fpga_interconnect_record; */
+    signal boost_control_bus_out : fpga_interconnect_record;
+    signal model_trigger_counter : natural range 0 to 255          := 0;
 
     constant cl_parameters : boost_model_parameters_record := (
         inductance  => 500.0e-6 ,
@@ -54,10 +137,9 @@ architecture rtl of efinix_top is
         rl          => 100.0e-3 ,
         timestep    => 1.0e-6);
 
-    signal boost_interface       : boost_interface_record;
-    signal control_counter       : natural range 0 to 2**15-1      := 0;
-    signal model_trigger_counter : natural range 0 to 255          := 0;
-    signal reference_voltage     : integer range -2**15 to 2**15-1 := to_fixed(205.0,7);
+    signal boost_control_ready : boolean := false;
+    signal duty_ratio : natural range 0 to 2**16-1;
+
 
 ------------------------------------------------------------------------
 begin
@@ -87,41 +169,13 @@ begin
 ------------------------------------------------------------------------
 
     test_control : process(core_clock)
-        constant dutymax : integer := to_fixed(0.90, number_of_fractional_bits => 15);
-        constant dutymin : integer := to_fixed(0.10, number_of_fractional_bits => 15);
-        constant vkp     : integer := to_fixed(0.5     , 15);
-        constant vki     : integer := to_fixed(0.016125/4.0 , 15);
     begin
         if rising_edge(core_clock) then
 
-            init_bus(bus_from_test_control);
-            connect_data_to_address(bus_from_communications, bus_from_test_control, tubepsu_addresses_pkg.reference_voltage_address, reference_voltage);
-
-            create_multiplier(voltage_multiplier);
-            create_voltage_control(self, voltage_multiplier,
-            proportional_gain => vkp,
-            integral_gain     => vki);
-
-            create_divider_and_multiplier(divider,divider_multiplier);
-            create_multiplier(multiplier);
-            create_current_control(current_control,multiplier, divider, divider_multiplier,
-                                    get_measurement(boost_interface, dc_link_voltage),
-                                    get_measurement(boost_interface, input_voltage),
-                                    dutymax     ,
-                                    dutymin);
 
             create_boost_interface(boost_interface);
-
-            if control_counter < 128e6/120e3 then
-                control_counter <= control_counter + 1;
-            else
-                control_counter <= 0;
-                request_current_control(current_control, self.current_ref, get_measurement(boost_interface,inductor_current)*2**4);
-                request_voltage_control(self, reference_voltage , get_measurement(boost_interface,dc_link_voltage)*2);
-            end if;
-
-            if current_control_is_ready(current_control) then
-                set_duty(boost_interface, get_int_multiplier_result(multiplier, 7, 20, target_radix => 15));
+            if boost_control_ready then
+                set_duty(boost_interface, duty_ratio);
             end if;
 
             if model_trigger_counter < integer(cl_parameters.timestep*128.0e6) then -- counter for 1us calculation time
@@ -133,6 +187,20 @@ begin
 
         end if; --rising_edge
     end process test_control;	
+
+    u_boost_control : entity work.boost_control
+    port map (
+        core_clock => core_clock,
+
+        boost_control_bus_in  => bus_from_communications,
+        boost_control_bus_out => boost_control_bus_out,
+        inductor_current      => get_measurement(boost_interface,inductor_current)*2**4,
+        input_voltage         => get_measurement(boost_interface,inductor_current),
+        dc_link_voltage       => get_measurement(boost_interface,dc_link_voltage),
+        boost_control_ready   => boost_control_ready,
+        duty_ratio            => duty_ratio
+    );
+
 
 ------------------------------------------------------------------------
     u_boost_model : entity work.boost_model
