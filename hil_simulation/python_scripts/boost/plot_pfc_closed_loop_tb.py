159e8d18decd37b3f172fcd74ca02103aae9c52d
Jari Honkanen <jaarhonka@yahoo.com>
2024-07-22 07:42:14 +0300

    add plot script for pfc closed loop

---
+++ hil_simulation/python_scripts/boost/plot_pfc_closed_loop_tb.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+#make this file root for relative paths
+import os
+path_to_this_file = os.path.dirname(os.path.realpath(__file__))
+
+#change directory to the lc filter directory
+
+fig1, (axT, axB) = plt.subplots(2,1,sharex=True,constrained_layout=True)
+
+# load vhdl simulation data
+vhdl_data = pd.read_csv('./pfc_closed_loop_tb.dat', delim_whitespace=True)
+
+vhdl_data.plot(ax=axT, x="time", y="volt", label="vhdl voltage")
+vhdl_data.plot(ax=axT, x="time", y="rvin", label="input voltage")
+vhdl_data.plot(ax=axB, x="time", y="curr", label="vhdl current")
+# vhdl_data.plot(ax=axB, x="time", y="iref", label="vhdl reference current")
+
+plt.show()
+plt.close('all')
